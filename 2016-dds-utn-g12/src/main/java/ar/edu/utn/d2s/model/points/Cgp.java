4bc55b33c8044798477dd3019647693f801ced04
Matias Mariano Minian <matias.minian@globant.com>
2016-10-27 12:08:16 -0300

    Implement method isOpen for CGP

--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/points/Cgp.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/points/Cgp.java
@@ -3,14 +3,14 @@ package ar.edu.utn.d2s.model.points;
 import ar.edu.utn.d2s.model.addres.Address;
 import org.uqbar.geodds.Point;
 
-import java.util.List;
+import java.util.*;
 
 public class Cgp extends PointOfInterest {
 
     private Commune commune;
-    private List<Service> services;
+    private Set<Service> services;
 
-    public Cgp(String name, String icon, Address address, Commune commune, List<Service> services) {
+    public Cgp(String name, String icon, Address address, Commune commune, Set<Service> services) {
         super(name, icon, address);
         this.commune = commune;
         this.services = services;
@@ -23,6 +23,20 @@ public class Cgp extends PointOfInterest {
         return commune.hasPointInside(point);
     }
 
+    @Override
+    public boolean isOpen(Calendar date, String serviceName) {
+        if (getServices().isEmpty()) {
+            throw new RuntimeException("CGP doesn't have services. Should have one at least.");
+        }
+
+        if (serviceName == null) {
+            return getServices().stream().anyMatch(service -> service.isOpen(date));
+        } else {
+            Optional<Service> serviceOptional = services.stream().filter(service -> service.getName().equals(serviceName)).findFirst();
+            return serviceOptional.orElseThrow(() -> new IllegalArgumentException("Field serviceName must have a valid name of a service.")).isOpen(date);
+        }
+    }
+
     //********** GETTERS & SETTERS **********//
 
     public Commune getCommune() {
@@ -33,11 +47,14 @@ public class Cgp extends PointOfInterest {
         this.commune = commune;
     }
 
-    public List<Service> getServices() {
+    public Set<Service> getServices() {
+        if (services == null) {
+            return new HashSet<>();
+        }
         return services;
     }
 
-    public void setServices(List<Service> services) {
+    public void setServices(Set<Service> services) {
         this.services = services;
     }
 }
--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/points/Service.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/points/Service.java
@@ -1,13 +1,37 @@
 package ar.edu.utn.d2s.model.points;
 
+import java.util.Calendar;
+
 public class Service {
 
+    private String name;
     // TODO Change it for corresponding object
     private String AttentionSchedule;
 
     // TODO Constructor
 
-    // TODO Getters & Setters
+    //********** METHODS **********//
 
+    public boolean isOpen(Calendar date) {
+        // TODO Implement this method
+        return false;
+    }
 
+    //********** GETTERS & SETTERS **********//
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAttentionSchedule() {
+        return AttentionSchedule;
+    }
+
+    public void setAttentionSchedule(String attentionSchedule) {
+        AttentionSchedule = attentionSchedule;
+    }
 }
