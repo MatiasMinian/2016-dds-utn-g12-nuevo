dadff5a279557b10eb6bbf46bcd2405ecfe51dc7
Matias Minian <minian.matias@gmail.com>
2016-10-30 17:21:44 -0300

    Create package database and class DatabaseMock inside. Commentaries for PointOfInteresConfig and DatabaseMock

---
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/database/DatabaseMock.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.d2s.database;
+
+public class DatabaseMock {
+
+    /**
+     * TODO
+     * Change this varibles for daos when the database is implemented. In the meantime services should get
+     * the information from this static varibles to perform requests.
+     */
+}
--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/config/PointOfInterestConfig.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/config/PointOfInterestConfig.java
@@ -2,6 +2,11 @@ package ar.edu.utn.d2s.model.config;
 
 public class PointOfInterestConfig {
 
+    /**
+     * This is a simulation of a NoSql Database, which we would have used for
+     * saving static data such as close range for different points of interest
+      */
+
     public static final double POI_CLOSE_RANGE = 0.5;
     public static final double BUS_CLOSE_RANGE = 0.1;
 }
