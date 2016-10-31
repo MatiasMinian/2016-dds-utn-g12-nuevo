ddf773df98eee5d864026560a634a7d65bf81272
Matias Mariano Minian <matias.minian@globant.com>
2016-10-31 12:14:16 -0300

    Test class TimeSchedule

---
+++ 2016-dds-utn-g12/src/test/java/ar/edu/utn/d2s/model/openhours/TimeScheduleTest.java
@@ -0,0 +1,39 @@
+package ar.edu.utn.d2s.model.openhours;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TimeScheduleTest {
+
+    private TimeSchedule timeSchedule;
+
+    @Before
+    public void setUp() throws Exception {
+
+        Map<DayEnum, WorkHours> openHours = new HashMap<>();
+        openHours.put(DayEnum.MONDAY, new WorkHours(LocalTime.of(10, 15), LocalTime.of(18, 0)));
+        openHours.put(DayEnum.WEDNESDAY, new WorkHours(LocalTime.of(11,0), LocalTime.of(17,0)));
+        timeSchedule = new TimeSchedule(openHours);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void isOpenHour() throws Exception {
+        assertTrue(timeSchedule.isOpenHour(DayEnum.MONDAY, LocalTime.of(14, 0)));
+        assertTrue(timeSchedule.isOpenHour(DayEnum.WEDNESDAY, LocalTime.of(11, 15)));
+
+        assertFalse(timeSchedule.isOpenHour(DayEnum.MONDAY, LocalTime.of(10, 0)));
+        assertFalse(timeSchedule.isOpenHour(DayEnum.FRIDAY, LocalTime.of(12, 0)));
+    }
+}
