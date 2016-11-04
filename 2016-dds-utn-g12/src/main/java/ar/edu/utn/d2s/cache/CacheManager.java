8136c3d9a3c771c114528963150b4ed4078fa064
Matias Mariano Minian <matias.minian@globant.com>
2016-11-04 11:10:45 -0300

    Finish implementing CacheManager

--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/cache/CacheManager.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/cache/CacheManager.java
@@ -11,9 +11,11 @@ public class CacheManager {
      * Used for managing caches, general cache and user caches.
      * General cache: For caching all searches made in the entire app by any user.
      * User cache: For caching searches made by a specific user.
+     * TODO If there's time, imlement user cache.
      */
 
     private static CacheManager instance = null;
+    private Cache<String, List<PointOfInterest>> cache = new Cache<>(CacheConfig.GENERAL_CACHE_SIZE);
 
     public static CacheManager getInstance() {
         if (instance == null) {
@@ -22,7 +24,11 @@ public class CacheManager {
         return instance;
     }
 
-    //private Cache<String, List<PointOfInterest>>
+    public List<PointOfInterest> matchSearch(String search) {
+        return cache.get("search");
+    }
 
-    // TODO Finish CacheManager with its caches
+    public void addSearch(String search, List<PointOfInterest> points) {
+        cache.put(search, points);
+    }
 }
