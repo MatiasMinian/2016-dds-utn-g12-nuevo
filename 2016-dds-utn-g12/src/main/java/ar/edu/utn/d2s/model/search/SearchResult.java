acd67cb5b708d82def3c09a6a05da27e107a0bf6
Matias Mariano Minian <matias.minian@globant.com>
2016-11-09 15:03:45 -0300

    Add points to SearchResult and username to User

--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/search/SearchResult.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/search/SearchResult.java
@@ -1,15 +1,22 @@
 package ar.edu.utn.d2s.model.search;
 
+import ar.edu.utn.d2s.model.points.PointOfInterest;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class SearchResult {
 
     private String text;
     private int numberOfResults;
     private long searchTime;
+    private Set<PointOfInterest> points;
 
-    public SearchResult(String text, int numberOfResults, long searchTime) {
+    public SearchResult(String text, int numberOfResults, long searchTime, Set<PointOfInterest> points) {
         this.text = text;
         this.numberOfResults = numberOfResults;
         this.searchTime = searchTime;
+        this.points = points;
     }
 
     public String getText() {
@@ -37,5 +44,16 @@ public class SearchResult {
 
     public void setSearchTime(long searchTime) {
         this.searchTime = searchTime;
+    }
+
+    public Set<PointOfInterest> getPoints() {
+        if (points == null) {
+            points = new HashSet<>();
+        }
+        return points;
+    }
+
+    public void setPoints(Set<PointOfInterest> points) {
+        this.points = points;
     }
 }
--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/users/User.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/users/User.java
@@ -3,17 +3,17 @@ package ar.edu.utn.d2s.model.users;
 public abstract class User {
 
     protected Long id;
-    protected String name;
+    protected String username;
 
     public Long getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
