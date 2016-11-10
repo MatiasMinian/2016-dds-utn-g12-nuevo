bbd3658832e8974aa652a700e945d194944e2358
Matias Mariano Minian <matias.minian@globant.com>
2016-11-10 14:57:40 -0300

    Add actions to Terminal}

--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/users/Terminal.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/users/Terminal.java
@@ -1,14 +1,18 @@
 package ar.edu.utn.d2s.model.users;
 
+import ar.edu.utn.d2s.model.actions.Action;
 import ar.edu.utn.d2s.model.search.SearchResult;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Terminal extends User {
 
     private boolean storeSearchResults;
     private List<SearchResult> searchResults;
+    private Set<Action> actions;
 
     public void addSearchResult(SearchResult searchResult) {
         searchResults.add(searchResult);
@@ -35,5 +39,16 @@ public class Terminal extends User {
 
     public void setSearchResults(List<SearchResult> searchResults) {
         this.searchResults = searchResults;
+    }
+
+    public Set<Action> getActions() {
+        if (actions == null) {
+            actions = new HashSet<>();
+        }
+        return actions;
+    }
+
+    public void setActions(Set<Action> actions) {
+        this.actions = actions;
     }
 }
