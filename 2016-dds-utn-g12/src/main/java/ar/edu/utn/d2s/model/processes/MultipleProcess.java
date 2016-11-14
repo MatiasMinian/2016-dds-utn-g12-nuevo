a8a6b0a0ee786c5fddf5aae97c35e043a0a9d68e
Matias Mariano Minian <matias.minian@globant.com>
2016-11-14 15:35:55 -0300

    Finish with Process and MultipleProcess

--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/processes/MultipleProcess.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/processes/MultipleProcess.java
@@ -1,22 +1,28 @@
 package ar.edu.utn.d2s.model.processes;
 
+import ar.edu.utn.d2s.model.users.Administrator;
+
 import java.util.HashSet;
 import java.util.Set;
 
-public class MultipleProcess implements Process {
+public class MultipleProcess extends Process {
 
     private Set<Process> processes = new HashSet<>();
 
-    @Override
-    public void run() {
-
+    public MultipleProcess(Administrator administrator) {
+        super(administrator);
     }
 
-
+    @Override
+    public void execute(Administrator administrator) {
+        processes.forEach(Process::run);
+    }
 
     public void addProcess(Process process) {
         processes.add(process);
     }
 
-
+    public void deleteProcess(Process process) {
+        processes.remove(process);
+    }
 }
--- 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/processes/Process.java
+++ 2016-dds-utn-g12/src/main/java/ar/edu/utn/d2s/model/processes/Process.java
@@ -1,4 +1,18 @@
 package ar.edu.utn.d2s.model.processes;
 
-public interface Process extends Runnable {
+import ar.edu.utn.d2s.model.users.Administrator;
+
+public abstract class Process implements Runnable {
+
+    private Administrator administrator;
+
+    public Process(Administrator administrator) {
+        this.administrator = administrator;
+    }
+
+    public void run() {
+        execute(administrator);
+    }
+
+    public abstract void execute(Administrator administrator);
 }
